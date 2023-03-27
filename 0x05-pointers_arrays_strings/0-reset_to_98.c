ec9bd63f0dfb000ce011755fc73656e231e849f2
joshuakoome <joshua.koome@outlook.com>
2023-03-27 13:25:04 -0700

    pointer to an int as parameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates
+ * reset_to_98 - change the value of n to 98
  * the value in points to to 98.
  *
  * @n: pointer to n
