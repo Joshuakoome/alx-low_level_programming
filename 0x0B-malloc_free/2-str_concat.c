34f129f4c851004897b81fbe2cedc1625696efa6
joshuakoome <joshua.koome@outlook.com>
2023-04-12 13:05:53 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,21 +1,39 @@
-#include <stdio.h>
-
+#include "main.h"
+#include <stdlib.h>
 
 /**
-* main - prints all arguments it receives.
-* @argc: argument count
-* @argv: arguments
-*
-* Return: 0
-*/
-int main(int argc, char **argv)
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ *
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
+ */
+char *str_concat(char *s1, char *s2)
 {
-int i;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
+	if (s1 == NULL)
+		s1 = "";
 
-for (i = 0; i < argc; i++)
-printf("%s\n", argv[i]);
+	if (s2 == NULL)
+		s2 = "";
 
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
 
-return (0);
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
