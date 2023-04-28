46e4a260c7484693be5122f6bdfd33e8d4511bca
joshuakoome <joshua.koome@outlook.com>
2023-04-28 11:31:37 -0700

    adds a new node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,41 +1,38 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
-
+#include <string.h>
 /**
-  * *add_node - adds a new node at the beginning of a list_t list.
-  * @head: pointer to first elements.
-  * @str: string.
-  * Return: the address of the new elements,
-  * or NULL if it failed.
-  */
-
+ *add_node - adds a new node at the begining
+ *         - of list_t list
+ *@head: a ptr to the head of list_t list
+ *@str:the string to be added to list_t list
+ *
+ *Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode;
+	char *dup;
+	int len;
+	list_t *nptr;
 
-	newNode = (list_t *)malloc(sizeof(list_t));
-	if (newNode == NULL)
+	nptr = malloc(sizeof(list_t));
+	if (nptr == NULL)
 		return (NULL);
-	newNode->str = strdup(str);
-	newNode->len = string_length(str);
-	newNode->next = *head;
-	*head = newNode;
 
-	return (*head);
-}
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(const char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
+	dup = strdup(str);
+	if (dup == NULL)
 	{
-		c++;
+		free(nptr);
+		return (NULL);
 	}
-	return (c);
+	for (len = 0; str[len]; len++)
+	{
+		;
+	}
+
+	nptr->str = dup;
+	nptr->len = len;
+	nptr->next = *head;
+
+		*head = nptr;
+	return (nptr);
 }
