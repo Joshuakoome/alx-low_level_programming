05188eb29bfed7d236a1a289e83718cda437d093
joshuakoome <joshua.koome@outlook.com>
2023-04-28 10:53:30 -0700

    adds a new node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,36 @@
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <string.h>
 
 /**
-  * *add_node_end - adds a new node at the end of a linked list.
-  * @head: start of the list.
-  * @str: string.
-  * Return: the address of the new element,
-  * or null if it failed.
-  */
+ * add_node_end - function to add node to the end of a linked list
+ * @head: pointer to the head of the list
+ * @str: the data of the node
+ *
+ * Return: pointer to the new node
+ * or NULL is it fails
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
+	list_t *new;
 
-	newNode = (list_t *)malloc(sizeof(list_t));
-	if (newNode == NULL)
+	while (*head != NULL)
+	{
+		/**
+		 * instead of modifying the head,
+		 * we modify the pointer to it;
+		 * this way the pointer to the start remains thesame,
+		 * but the pointer to the pointer to the start changes.
+		*/
+		head = &((*head)->next);
+	}
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	newNode->str = strdup(str);
-	newNode->len = string_length(str);
-	newNode->next = NULL;
-	/* check if list is null and make new node the head */
-	if (*head == NULL)
-		*head = newNode;
-	/* else find last node and add new node */
-	else
-	{
-		list_t *lastNode = *head;
-		/* last node has to be NULL use while to check */
-		while (lastNode->next != NULL)
-		{
-			lastNode = lastNode->next;
-		}
-		/* if null is found add new node at the end */
-		lastNode->next = newNode;
-	}
-	return (newNode);
-}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	*head = new;
 
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(const char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
+	return (new);
 }
